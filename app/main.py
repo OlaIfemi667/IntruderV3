0f8cfdfa528a997e17b8fa82559f4a2a0c3e1cab
ola-iya <bancolejeandedieu@gmail.com>
2025-05-06 11:24:48 +0100

    feat: flask app base structure

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+
+#my packages
+from subprocessUtils.subprocess import * #for  subprocess handling
+from parsing.whoisParsing import * #to parse whois output
+from parsing.nmapParsing import * #to parse nmap
+from commandsFunctions.ipFunction import * #logique when only ip is provide
+from database.database import * #for db SQLite operations
+
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def index():
+    return render_template("home.html")
